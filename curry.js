1efc81247e4dce38c42a8720c19ec1fdcd2c9b71
Elfi Yang <elfiyang16@gmail.com>
2020-08-23 14:21:47 +0100

    feat: compose

--- curry.js
+++ curry.js
@@ -1,25 +1,30 @@
-const {curry} = require('ramda')
+const { curry } = require("ramda");
 
-const add = (x, y) => x + y
+// const add = (x, y) => x + y;
 
-// const curry = f =>
-//   x => y => f(x, y)
+// const curry = (f) => (x) => (y) => f(x, y);
+// const uncurry = (f) => (x, y) => f(x)(y);
+// const curriedAdd = curry(add);
 
-const modulo = curry((x, y) => y % x)
+// const modulo = curry((x, y) => y % x);
 
-const isOdd = modulo(2)  // (2, y) => 2 % y
+// const isOdd = modulo(2); // (2, y) => 2 % y  as now x = 2
+// const result = isOdd(3); //false
 
+// const filter = curry((f, xs) => xs.filter(f));
+// the sequence matters: can't have
+// const filter = curry((xs, f) => f.filter(xs));
+//1st arg: to be remembered; 2nd arg: input
 
-const filter = curry((f, xs) => xs.filter(f))
+const getOdds = filter(isOdd);
+const toFiltered = getOdds([1, 2, 3, 4, 5]);
 
-const getOdds = filter(isOdd)
+// const replace = curry((regex, replacement, str) =>
+//   str.replace(regex, replacement)
+// );
 
-const replace = curry((regex, replacement, str) =>
-  str.replace(regex, replacement)
-)
+// const replaceVowels = replace(/[AEIOU]/gi, "!");
 
-const replaceVowels = replace(/[AEIOU]/ig, '!')
+// const result = replaceVowels("Hey I have words");
 
-const result = replaceVowels('Hey I have words')
-
-console.log(result)
+// console.log(result);
