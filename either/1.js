91b6a3e69ebc4a96dfae31a195fedd22007a5196
Elfi Yang <elfiyang16@gmail.com>
2020-08-28 17:51:07 +0100

    feat: functors exercise

--- either/1.js
+++ either/1.js
@@ -1,5 +1,22 @@
-const findColor = name =>
+const Right = (x) => ({
+  map: (f) => Right(f(x)),
+  fold: (f, g) => g(x),
+  inspect: () => `Right(${x})`,
+});
+
+const Left = (x) => ({
+  map: (f) => Left(x),
+  fold: (f, g) => f(x),
+  inspect: () => `Left(${x})`,
+});
+
+const findColor = (name) =>
   ({ red: "#ff4444", blue: "#3b5998", yellow: "#fff68f" }[name]);
+
+const findColor = (name) => {
+  const found = { red: "#ff4444", blue: "#3b5998", yellow: "#fff68f" }[name];
+  return found ? Right(found) : Left("dunno");
+};
 
 const res = findColor("red");
 
